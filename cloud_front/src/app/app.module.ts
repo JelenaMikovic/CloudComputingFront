b0dfe8861c2e0a96f6258a64e2dafbb2f68e8b71
Jana Nikolic <jananikolic25@gmail.com>
2023-04-29 15:07:30 +0200

    Added component for main page and navbar

--- cloud_front/src/app/app.module.ts
+++ cloud_front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    MainPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
