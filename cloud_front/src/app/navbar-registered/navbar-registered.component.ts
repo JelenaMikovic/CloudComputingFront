87954524d2cd9316e9260b763f814ac3c730b301
Jana Nikolic <jananikolic25@gmail.com>
2023-04-29 16:28:03 +0200

    Added navbar for registered user

---
+++ cloud_front/src/app/navbar-registered/navbar-registered.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-registered',
+  templateUrl: './navbar-registered.component.html',
+  styleUrls: ['./navbar-registered.component.css']
+})
+export class NavbarRegisteredComponent {
+
+}
