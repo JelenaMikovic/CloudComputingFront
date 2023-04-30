24497024a9b2757ba54a24bb047ac0cbca054635
Katarina <mirkovicka01@gmail.com>
2023-04-30 16:49:30 +0200

    missing import

--- cloud_front/src/app/app-routing.module.ts
+++ cloud_front/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationComponent },
