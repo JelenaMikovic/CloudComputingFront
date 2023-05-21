9ad3a89397175032972ffa753a69de7cb53cdefd
Jana Nikolic <jananikolic25@gmail.com>
2023-05-21 17:03:10 +0200

    Added guard

--- cloud_front/src/app/app-routing.module.ts
+++ cloud_front/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoginComponent } from './login/login.component';
+import { LoginGuard } from 'src/guard/LoginGuard';
+import { NotLoggedInGuard } from 'src/guard/NotLoggedInGuard';
 
 
 const routes: Routes = [
-  { path: 'registration', component: RegistrationComponent },
-  { path: 'home', component: MainPageComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'registration', component: RegistrationComponent, canActivate: [LoginGuard] },
+  { path: 'home', component: MainPageComponent, canActivate: [NotLoggedInGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard]}, 
+  { path: '**', component: MainPageComponent, canActivate: [NotLoggedInGuard, LoginGuard]},
+  { path: '', component: MainPageComponent, pathMatch: 'full', canActivate: [NotLoggedInGuard, LoginGuard]}
 ];
 
 @NgModule({
---
+++ cloud_front/src/guard/LoginGuard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.authService.getUser().then((res) => {
+        console.log(res);
+        
+      if(res){
+        this.router.navigate(['home']);
+        return false;
+      }
+      return true;
+    });
+    return true;
+  }
+}
